88c7ea735fc95b53b44d09d3fe3918183f3a066e
Adrián García <65560724+adrian-gg@users.noreply.github.com>
2025-03-18 14:42:44 +0100

    [add] challenge #76-cifrado-cesar

---
+++ #76-cifrado-cesar/challenge.md
@@ -0,0 +1,13 @@
+# Reto #76: Cifrado César
+
+#### Dificultad: Fácil
+
+## Enunciado
+
+```
+Crea un programa que realize el cifrado César de un texto y lo imprima.
+También debe ser capaz de descifrarlo cuando así se lo indiquemos.
+
+Te recomiendo que busques información para conocer en profundidad cómo
+realizar el cifrado. Esto también forma parte del reto.
+```
---
+++ #76-cifrado-cesar/script.js
@@ -0,0 +1,30 @@
+const removeAccents = (text) =>
+  text
+    .replace(/ñ/g, "#")
+    .replace(/Ñ/g, "%")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/#/g, "ñ")
+    .replace(/%/g, "Ñ")
+
+function caesarCipher(text, shift, decrypt = false) {
+  const formatedText = removeAccents(text).toLowerCase()
+  let chars = "abcdefghijklmnñopqrstuvwxyz"
+
+  if (decrypt) chars = chars.split("").reverse().join("")
+
+  const result = formatedText
+    .split("")
+    .map((char) => {
+      const indexChar = chars.indexOf(char)
+      indexChar >= 0 ? chars[(indexChar + shift) % chars.length] : char
+    })
+    .join("")
+
+  console.log(result)
+}
+
+caesarCipher("Mi nombre es Adrián.", 3)
+caesarCipher("ol proeuh hv dguldp.", 3, true)
+caesarCipher("Mi nombre es Adrián.", 5)
+caesarCipher("qn rtqgwj jx fiwnfr.", 5, true)
