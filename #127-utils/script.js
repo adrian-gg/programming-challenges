dcd1bdee8310355887cded8f6ce1dca7237cfa72
adrian-gg <garciagregoriadrian@gmail.com>
2025-07-02 19:34:22 +0200

    [fix-error] challenge #127-utils

--- #127-utils/script.js
+++ #127-utils/script.js
@@ -1,6 +1,6 @@
 //numbers
 export const getRandomNumber = (max, min = 0) =>
-  Math.floor(Math.random() * (max + min) + 1 - min)
+  Math.floor(Math.random() * (max - min + 1)) + min
 
 //strings
 export const toNormalize = (str) => {
